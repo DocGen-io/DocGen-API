2d03d8f0c0774c76374602afd27e9c9b2c1a962e
AliSaleemHasan <ali1saleem1hasan@gmail.com>
2026-04-07 23:03:37 +0200

    Add dynamic update function that works with different models

--- api/repositories/base.py
+++ api/repositories/base.py
@@ -26,6 +26,24 @@ class BaseRepository(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         await db.refresh(db_obj)
         return db_obj
 
+    async def update(
+        self, db: AsyncSession, *, db_obj: ModelType, obj_in: UpdateSchemaType | dict
+    ) -> ModelType:
+        obj_data = db_obj.__dict__
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.model_dump(exclude_unset=True)
+            
+        for field in obj_data:
+            if field in update_data:
+                setattr(db_obj, field, update_data[field])
+        
+        db.add(db_obj)
+        await db.commit()
+        await db.refresh(db_obj)
+        return db_obj
+
     async def delete(self, db: AsyncSession, *, id: str) -> Optional[ModelType]:
         obj = await self.get(db, id)
         if obj:
